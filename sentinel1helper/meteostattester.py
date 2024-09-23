fb6511c954866acfc9192913cd206de05a1489d2
hog <dieter@hoogestraat.dk>
2024-09-23 13:20:43 +0200

    now ready to become a function

--- sentinel1helper/meteostattester.py
+++ sentinel1helper/meteostattester.py
@@ -17,15 +17,17 @@ df = pd.read_csv(in_file)
 print(df.iloc[0]['X'])
 from pyproj import Transformer 
 transformer = Transformer.from_crs(25832, 4326) 
-y,x = transformer.transform(df.iloc[0]['X'], df.iloc[0]['Y'])
-print(x,y)
+ps_lat,ps_lon = transformer.transform(df.iloc[0]['X'], df.iloc[0]['Y'])
+print(ps_lon, ps_lat)
 # Set time period
+ps_loc = Point(ps_lat, ps_lon,df.iloc[0]['Z'])
 start = datetime(2016, 4, 6)
 end = datetime(2021, 12, 30)
 
 stations = Stations()
-stations = stations.nearby(y, x)
+stations = stations.nearby(ps_loc._lat, ps_loc._lon)
 station = stations.fetch()
+
 # stations = Stations()
 # stations = stations.nearby(54.3833, 10.15)
 # station = stations.fetch()
@@ -34,14 +36,14 @@ print(station.columns)
 print(station.head(10))
 print(station.iloc[0])
 
-
-
+data = Daily(ps_loc, start, end)
+data = data.fetch()
 # Get daily data
 #
-# # Plot line chart including average, minimum and maximum temperature
-# data.plot(y=['tavg', 'tmin', 'tmax'])
-#
-# print(data.head())
+# Plot line chart including average, minimum and maximum temperature
+data.plot(y=['tavg', 'tmin', 'tmax'])
+plt.show()
+print(data.head())
 # print(data.index)
 # omega_g = 1. / 180
 # fs = 1. / 1
