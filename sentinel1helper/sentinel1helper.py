572747649139dd7caad0c933e42cebf583ca1a09
hog <dieter@hoogestraat.dk>
2024-09-25 14:15:58 +0200

    built basic functionality

--- sentinel1helper/sentinel1helper.py
+++ sentinel1helper/sentinel1helper.py
@@ -6,26 +6,43 @@ Created on 14. sep. 2024
 from datetime import datetime
 
 import geopandas as gpd
+import datetime as dt
 import numpy as np
 import re
 from numpy import datetime64
 
 datepattern = 'date_\d{8}'
 
+
+
 class gmdata():
     
     def __init__(self, gmdatafile,
-                        layer,
-                        engine='fiona'):
+                        layer  = None,
+                        engine = 'fiona',
+                        cycle  = 6):
         
-        self.data = self.read_geofile(gmdatafile, layer, engine)
+        self.__data = self.read_geofile(gmdatafile, layer, engine)
         
-        self.dt_dats, 
-        self.dats, 
-        self.nodats,
-        self.dt_dats_asDays = self.get_datetime_dates(self.data.columns)
+        self.dt_dats,\
+        self.dats,\
+        self.nodats, \
+        self.dt_dats_asDays = self.get_datetime_dates(self.__data.columns)
+        
+        self.dt_dats_padded = []
+        self.dt_dats_padded_asDays = []
+        self.pad_days(cycle)
+        
+    @property
+    def len(self):
+        return len(self.__data)
+    
+    @property
+    def data(self):
+        return self.__data
         
     def read_geofile(self, geofile, layer = None, engine='fiona'):
+
         
         cached_engine = gpd.options.io_engine
         gpd.options.io_engine = engine
@@ -39,8 +56,20 @@ class gmdata():
         
         return out_file
 
-
-
+    def pad_days(self, cycle = 6):
+        dt_dats_padded = []
+        dt_dats_padded_asDays = [0]
+        old_date = self.dt_dats[0]
+        while old_date <= self.dt_dats[-1]:
+            dt_dats_padded.append(old_date)
+            old_date = old_date + dt.timedelta(cycle)
+            
+        for i in dt_dats_padded[1:]:
+            dt_dats_padded_asDays.append((i - dt_dats_padded[0]).days)
+            
+        self.dt_dats_padded         = dt_dats_padded
+        self.dt_dats_padded_asDays  = dt_dats_padded_asDays
+        
     def get_datetime_dates(self, in_list):
         
         out_dt_dats = []
@@ -61,6 +90,8 @@ class gmdata():
         return out_dt_dats, out_dats, out_nodats, out_dt_dats_asDays
 
 
+def pad_ts():
+    return
 
 
 def get_numpy64_dates(in_list):
