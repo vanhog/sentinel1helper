5a4ccfb7d6631cef2278b17b238739a041781443
vanhog <dieter@hoogestraat.dk>
2024-12-06 12:42:55 +0100

    filtered variant

--- sentinel1helper/kite_covcalc.py
+++ sentinel1helper/kite_covcalc.py
@@ -2,14 +2,16 @@ from kite import Scene
 
 # This script calculates the covariance of a kite scene
 
+kitescene_path = '/media/hog/hogsandisc/data/mscthesisdata/insar_float32_losturn_v3/'
 
-files = ['/media/hog/Iomega HDD/mscdata/tl5_l2b_a_044_01_mscaoi_f32_foccov_losturn',\
-         '/media/hog/Iomega HDD/mscdata/tl5_l2b_a_117_02_mscaoi_f32_foccov_losturn',\
-         '/media/hog/Iomega HDD/mscdata/tl5_l2b_d_066_02_mscaoi_f32_foccov_losturn',\
-         '/media/hog/Iomega HDD/mscdata/tl5_l2b_d_139_01_mscaoi_f32_foccov_losturn']
+files = ['tl5_l2b_a_044_01_mscaoi_f32_foccov_losturn',\
+         'tl5_l2b_a_117_02_mscaoi_f32_foccov_losturn',\
+         'tl5_l2b_d_066_02_mscaoi_f32_foccov_losturn',\
+         'tl5_l2b_d_139_01_mscaoi_f32_foccov_losturn']
 
 
-for kitescene in files:
+for kitescenefilename in files:
+    kitescene = kitescene_path + kitescenefilename
     scene = Scene.load(kitescene)
     print('working on: t', kitescene)
     covfull = scene.covariance.covariance_matrix
